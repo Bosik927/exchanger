8f29af1518f2c7ec7e26fce4fab3334730d6b3a9
Bosik927 <bosik927@gmail.com>
2024-11-04 22:30:37 +0100

    Init project

---
+++ src/main/kotlin/com/bosik927/exchanger/ExchangerApplication.kt
@@ -0,0 +1,11 @@
+package com.bosik927.exchanger
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExchangerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExchangerApplication>(*args)
+}
